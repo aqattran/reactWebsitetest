cba162fdd918985e3c0401636bf2afe6d0666670
aqattran <aqat.tran@gmail.com>
2022-05-19 00:29:28 -0400

    Adding names and randomization

--- project01/src/App.js
+++ project01/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /* const name = "Alice" */
+  const handleNameChange = () => {
+    const names = ['Alice', 'Mihir', 'Margaret', 'Megan'];
+    const int = Math.floor(Math.random()*4);
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing this out - {handleNameChange()}!
         </p>
         <a
           className="App-link"
@@ -17,6 +23,7 @@ function App() {
         >
           Learn React
         </a>
+     {/*  <p>{name}</p> */}
       </header>
     </div>
   );
