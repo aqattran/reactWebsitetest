ca28ea0a9ce8d7258c2801ad19cff6f4a5f4c295
aqattran <aqat.tran@gmail.com>
2022-05-20 00:29:05 -0400

    understanding events

--- project01/src/content.js
+++ project01/src/content.js
@@ -6,11 +6,29 @@ export const content = () => {
     const int = Math.floor(Math.random()*4);
     return names[int];
   } 
+
+  const handleClick = () => {
+    console.log('You clicked it')
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was here`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return (
     <main>
-        <p>
+        <p onDoubleClick = {handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button
+          onClick = {handleClick}>Click It </button>
+        <button onClick = {() => handleClick2('Alice')}>Click It </button>
+        <button onClick = {(e) => handleClick3(e)}>Click It </button>
+      
     </main>
   )
 }
